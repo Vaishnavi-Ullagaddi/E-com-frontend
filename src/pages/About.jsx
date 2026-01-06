886f9bc21b93e22fa3e567f6f59de5bc497bc5a7
Vaishnavi-Ullagaddi <vvaishnaviullagaddi@gmail.com>
2026-01-06 15:25:09 +0530

    initial commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return;
+  <div>
+    <h1>Sale 20% off on everything</h1>
+    <p>
+      Welcome to our store! We are committed to bringing you high-quality
+      products at affordable prices. Our goal is to make online shopping easy,
+      secure, and enjoyable. We carefully select every product to ensure the
+      best value and customer satisfaction. Thank you for choosing us as your
+      trusted shopping destination.
+    </p>
+  </div>;
 }
 
 export default About;
