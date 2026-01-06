b1a5dfbf770de4ab6e2c3e21e584d25345c4e5f0
Vaishnavi-Ullagaddi <vvaishnaviullagaddi@gmail.com>
2026-01-06 15:39:32 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Home() {
-  return <div>Home page</div>;
+  return;
+  <div>
+    <h1>Shop Quality Products at the Best Prices</h1>
+    <p>
+      Welcome to our online store, where quality meets convenience. We offer
+      carefully selected products, easy checkout, and reliable delivery to make
+      your shopping experience smooth and enjoyable
+    </p>
+    <b>Why Choose Us</b>
+    <li>✔ High-quality products</li>
+    <li>✔ Affordable prices</li>
+    <li>✔ Secure payments</li>
+    <li>✔ Fast & reliable delivery</li>
+    <li>✔ Customer-first support</li>
+  </div>;
 }
 
 export default Home;
